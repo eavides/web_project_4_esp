261fb3d0f422a478c92173c7e31d58d07b964e12
Ernesto Perez <ernesto.perezvides@gmail.com>
2023-04-18 19:13:04 -0600

    entrega proyecto7 revision

--- index.js
+++ index.js
@@ -72,39 +72,7 @@ initialCards.forEach((item) => {
   const cardElement = card.generateCard();
   document.querySelector(".grid__container").append(cardElement);
 });
-/*
-function openPopup() {
-  popup.classList.add("popup_opened");
-  namePop.value = profileName.textContent;
-  positionPop.value = positionProf.textContent;
-}
 
-function closeAllPopup() {
-  popup.classList.remove("popup_opened");
-  newcard.classList.remove("newcard_opened");
-  imgDisplay.classList.remove("imgdisplay_opened");
-}
-
-function closeOut(evt) {
-  if (
-    evt.target.classList.value == "popup popup_opened" ||
-    evt.target.classList.value == "newcard newcard_opened" ||
-    evt.target.classList.value == "imgdisplay__container"
-  ) {
-    closeAllPopup();
-  }
-  if (evt.key == "Escape") {
-    closeAllPopup();
-  }
-}
-
-function saveProfileInfo(evt) {
-  evt.preventDefault();
-  profileName.textContent = namePop.value;
-  positionProf.textContent = positionPop.value;
-  closeAllPopup();
-}
-*/
 function createCard() {
   newcard.classList.add("newcard_opened");
 }
